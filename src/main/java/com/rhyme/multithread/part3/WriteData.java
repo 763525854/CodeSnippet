b3aecff3a287c1665a0b8e18a8787d334a5f17f6
rhyme <763525854@qq.com>
2018-06-11 13:44:08 +0800

    线程通信，管道。

--- src/main/java/com/rhyme/multithread/part3/WriteData.java
+++ src/main/java/com/rhyme/multithread/part3/WriteData.java
@@ -25,11 +25,33 @@ public class WriteData {
 			System.out.println(outData);
 		}
 		System.out.println();
-		try {
+		try { 
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+		WriteData writeData = new WriteData();
+		ReadData readData = new ReadData();
+		PipedInputStream inputStream = new PipedInputStream();
+		PipedOutputStream outputStream = new PipedOutputStream();
+		try {
+			outputStream.connect(inputStream);
+ 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ThreadRead threadRead = new ThreadRead(readData, inputStream);
+		threadRead.start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		ThreadWrite threadWrite = new ThreadWrite(writeData, outputStream);
+		threadWrite.start();
+
 	}
 }
 
@@ -42,7 +64,7 @@ class ReadData {
 			leadLength = input.read(byteArray);
 			while (leadLength != -1) {
 				String newData = new String(byteArray, 0, leadLength);
-				System.out.println(newData);
+				System.out.println(newData+" what the fuck");
 				leadLength = input.read(byteArray);
 			}
 			System.out.println();
@@ -51,5 +73,38 @@ class ReadData {
 			e.printStackTrace();
 		}
 
+	}
+}
+
+class ThreadWrite extends Thread {
+	private WriteData write;
+	private PipedOutputStream out;
+
+	public ThreadWrite(WriteData write, PipedOutputStream out) {
+		super();
+		this.write = write;
+		this.out = out;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		write.writeMethod(out);
+	}
+}
+
+class ThreadRead extends Thread {
+	private ReadData read;
+	private PipedInputStream input;
+
+	public ThreadRead(ReadData read, PipedInputStream input) {
+		this.read = read;
+		this.input = input;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		read.readMethod(input);
 	}
 }

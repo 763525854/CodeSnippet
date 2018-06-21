d84fd3c4d8e54f335a49510066d1eb42086577da
rhyme <763525854@qq.com>
2018-06-21 15:54:18 +0800

    1.可见性

---
+++ src/main/java/com/rhyme/Java并发编程实战/part3/NoVisibility.java
@@ -0,0 +1,31 @@
+/**
+ *  Created by weiping.gong on 2018年6月21日
+ */
+package com.rhyme.Java并发编程实战.part3;
+
+/**
+ * @Author: weiping.gong
+ * @Description:
+ * @Date: created in 2018年6月21日
+ */
+public class NoVisibility {
+	private static boolean ready;
+	private static int number;
+
+	private static class ReadThread extends Thread {
+		@Override
+		public void run() {
+			super.run();
+			while (!ready) {
+				Thread.yield();
+				System.out.println(number);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new ReadThread().start();
+		number = 42;
+		ready = true;
+	}
+}

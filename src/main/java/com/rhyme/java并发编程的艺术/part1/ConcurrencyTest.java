7357d9e1f8ccc9e685b701cd2908cd741c5ccbec
rhyme <763525854@qq.com>
2018-06-27 10:09:58 +0800

    多线程一定快吗

---
+++ src/main/java/com/rhyme/java并发编程的艺术/part1/ConcurrencyTest.java
@@ -0,0 +1,54 @@
+/**
+ *  Created by weiping.gong on 2018年6月27日
+ */
+package com.rhyme.java并发编程的艺术.part1;
+
+/**
+ * @Author: weiping.gong
+ * @Description:
+ * @Date: created in 2018年6月27日
+ */
+public class ConcurrencyTest {
+	private static final long count = 10000000000L;
+
+	public static void main(String[] args) throws InterruptedException {
+		concuttency();
+		serial();
+	}
+
+	private static void concuttency() throws InterruptedException {
+		long start = System.currentTimeMillis();
+		Thread thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				int a = 0;
+				for (long i = 0; i < count; i++) {
+					a += 5;
+				}
+			}
+		});
+		thread.start();
+		int b = 0;
+		for (long i = 0; i < count; i++) {
+			b--;
+		}
+		long time = System.currentTimeMillis() - start;
+		thread.join();
+		System.out.println("concurrency:" + time + "ms,b=" + b);
+	}
+
+	private static void serial() {
+		long start = System.currentTimeMillis();
+		int a = 0;
+		for (long i = 0; i < count; i++) {
+			a += 5;
+		}
+		int b = 0;
+		for (long i = 0; i < count; i++) {
+			b--;
+		}
+		long time = System.currentTimeMillis() - start;
+		System.out.println("serial:" + time + "ms,b=" + b + ",a=" + a);
+	}
+}

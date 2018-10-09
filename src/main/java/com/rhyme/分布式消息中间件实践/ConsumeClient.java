20a9f8e79203a1cddd0a67be6d2a3aba2db7d62c
rhyme <rhyme@DESKTOP-UE1GSE1>
2018-10-09 14:28:35 +0800

    消费 消息

---
+++ src/main/java/com/rhyme/分布式消息中间件实践/ConsumeClient.java
@@ -0,0 +1,10 @@
+package com.rhyme.分布式消息中间件实践;
+//消费消息
+public class ConsumeClient {
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
+		MQClient client=new MQClient();
+		String message=client.consume();
+		System.out.println("获取的消息为:"+message);
+	}
+}

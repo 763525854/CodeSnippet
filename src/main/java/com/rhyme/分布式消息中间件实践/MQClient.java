593ad39b27220a8385339e6601fc77ab985f37b6
rhyme <rhyme@DESKTOP-UE1GSE1>
2018-10-09 14:24:15 +0800

    消息客户端

---
+++ src/main/java/com/rhyme/分布式消息中间件实践/MQClient.java
@@ -0,0 +1,28 @@
+package com.rhyme.分布式消息中间件实践;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+//消息客户端
+public class MQClient {
+	// 生产消息
+	public static void produce(String message) throws Exception {
+		Socket socket = new Socket(InetAddress.getLocalHost(), BrokerServer.SERVICE_PORT);
+		PrintWriter out = new PrintWriter(socket.getOutputStream());
+		out.println(message);
+		out.flush();
+	}
+
+	public static String consume() throws Exception {
+		Socket socket = new Socket(InetAddress.getLocalHost(), BrokerServer.SERVICE_PORT);
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(socket.getOutputStream());
+		out.println("CONSUME");
+		out.flush();
+		String message = in.readLine();
+		return message;
+	}
+}

2171ad2e3322d4d007fb457e3843a0e4ec62d219
依韵 <763525854@qq.com>
2020-10-30 15:41:47 +0800

    插入排序的两种写法以及效率对比，效率好像跟书籍说的不一样

---
+++ src/main/java/com/rhyme/java程序员面试笔试宝典/part8/插入排序.java
@@ -0,0 +1,46 @@
+package com.rhyme.java程序员面试笔试宝典.part8;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class 插入排序 {
+	public static void insertSort(int a[]) {
+		long time=System.currentTimeMillis();
+		for (int i = 0; i < a.length; i++) {
+			for (int j = i; j>0; j--) {
+				int temp=a[j-1];
+				if (a[j]<a[j-1]) {
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		System.out.println((System.currentTimeMillis()-time)+"s");
+	}
+	public static void main(String[] args) {
+		int a[]= {5,4,9,8,7,6,0,1,3,2};
+		int j=100000;
+		int[] b=new int[j];
+		int[] c=new int[j];
+		for (int i = 0; i < j; i++) {
+			b[i]=new Random().nextInt(j);
+		}
+		c=Arrays.copyOf(b, j);
+		insertSort(b);
+		insertSortA(c);
+	}
+	
+	public static void insertSortA(int a[]) {
+		long time=System.currentTimeMillis();
+		int j;
+		for (int i = 1; i < a.length; i++) {
+			Integer temp = a[i];
+			for (j = i; j > 0 && temp.compareTo(a[j - 1]) < 0; j--)
+				a[j] = a[j - 1];
+			a[j] = temp;
+		}
+		System.out.println((System.currentTimeMillis()-time)+"s");
+	}
+
+	
+}

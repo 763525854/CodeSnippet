4b312a3505ef06e88dcf2a2ed134ff1864da6cc1
依韵 <763525854@qq.com>
2020-11-03 01:11:05 +0800

    增加高效冒泡方法

--- src/main/java/com/rhyme/java程序员面试笔试宝典/part8/冒泡排序.java
+++ src/main/java/com/rhyme/java程序员面试笔试宝典/part8/冒泡排序.java
@@ -1,5 +1,6 @@
 package com.rhyme.java程序员面试笔试宝典.part8;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class 冒泡排序 {
@@ -7,9 +8,12 @@ public class 冒泡排序 {
 		int j = 100000;
 		int[] a = new int[j];
 		for (int i = 0; i < j; i++) {
-			a[i] = new Random().nextInt(j-1);
+			a[i] = new Random().nextInt(j - 1);
 		}
+		int[] b = new int[j];
+		b = Arrays.copyOf(a, j);
 		bubbleSort(a);
+		bubbleSortB(b);
 	}
 
 	public static void bubbleSort(int a[]) {
@@ -20,6 +24,23 @@ public class 冒泡排序 {
 				if (a[j] > a[j + 1]) {
 					a[j] = a[j + 1];
 					a[j + 1] = temp;
+				}
+			}
+		}
+		System.out.println((System.currentTimeMillis() - time) + "s");
+	}
+
+	public static void bubbleSortB(int a[]) {
+		long time = System.currentTimeMillis();
+		boolean needNextPass = true;
+		for (int i = 1; i < a.length && needNextPass; i++) {
+			needNextPass = false;
+			for (int j = 0; j < a.length - i; j++) {
+				int temp = a[j];
+				if (a[j] > a[j + 1]) {
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+					needNextPass = true;
 				}
 			}
 		}

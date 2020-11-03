50d6965fc8225a0fd67665e07139993999b4e587
依韵 <763525854@qq.com>
2020-11-03 23:49:13 +0800

    增加快速排序算法。

---
+++ src/main/java/com/rhyme/java程序员面试笔试宝典/part8/快速排序.java
@@ -0,0 +1,47 @@
+package com.rhyme.java程序员面试笔试宝典.part8;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class 快速排序 {
+	public static void main(String[] args) {
+		int j = 10;
+		int[] a = new int[j];
+		for (int i = 0; i < j; i++) {
+			a[i] = new Random().nextInt(j - 1);
+		}
+		int[] c = new int[j];
+		c = Arrays.copyOf(a, j);
+		quickSort(c, 0, c.length - 1);
+	}
+
+	public static void quickSort(int[] a, int left, int rihgt) {
+		int temp = a[left];
+		int i = left;
+		int j = rihgt;
+		while (i != j) {
+			while (a[j] >= temp && j > i) {
+				j--;
+			}
+			while (a[i] <= temp && j > i) {
+				i++;
+			}
+			if (i < j) {
+				int t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+			}
+		}
+		a[left] = a[i];
+		a[i] = temp;
+		if (left < i - 1) {
+			// 继续处理左边的数据，直到左边数据只剩1个，或者不剩数据
+			quickSort(a, left, i - 1);
+		}
+		if (rihgt > j + 1) {
+			// 继续处理右边的数据，直到右边的数据只剩1个，或者不剩数据
+			quickSort(a, i + 1, rihgt);
+		}
+	}
+
+}

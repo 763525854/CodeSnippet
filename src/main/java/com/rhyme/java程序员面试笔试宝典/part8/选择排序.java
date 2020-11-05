19c1b1aefcce30604cb189124247df50dd682588
依韵 <763525854@qq.com>
2020-11-05 16:03:52 +0800

    加入选择排序的中文解释，以及加深对代码的理解

--- src/main/java/com/rhyme/java程序员面试笔试宝典/part8/选择排序.java
+++ src/main/java/com/rhyme/java程序员面试笔试宝典/part8/选择排序.java
@@ -1,32 +1,48 @@
 package com.rhyme.java程序员面试笔试宝典.part8;
 
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 对于给定的一组记录，经过第一轮比较后得到最小的记录，然后将该记录与第一个记录的位置进行交换。
+ * 接着对不包括第一个的记录进行第二轮比较，得到最小的记录与第二个记录进行交换。重复该过程.
+ * 效率较低。10W数据，耗时2秒左右。50W数据耗时1分钟左右。100W数据，三分钟。
+ * 
+ * @author rhyme
+ */
 public class 选择排序 {
-	
+
 	public static void main(String[] args) {
-		int a[]= {5,4,9,8,7,6,0,1,3,2};
-		selectSort(a);
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
+		int j = 10;
+		int[] a = new int[j];
+		for (int i = 0; i < j; i++) {
+			a[i] = new Random().nextInt(j);
 		}
+		// int a[] = { 5, 4, 9, 8, 7, 6, 0, 1, 3, 2 };
+		System.out.println(Arrays.toString(a));
+		selectSort(a);
+		System.out.println(Arrays.toString(a));
 	}
+
 	public static void selectSort(int a[]) {
 		int i;
 		int j;
-		int temp=0;
-		int flag=0;
-		int n=a.length;
+		int temp = 0;
+		int flag = 0;
+		int n = a.length;
 		for (i = 0; i < n; i++) {
-			temp=a[i];
-			flag=i;
-			for (j = i+1; j < n; j++) {
-				if (a[j]<temp) {
-					temp=a[j];
-					flag=j;
+			temp = a[i];
+			flag = i;
+			for (j = i + 1; j < n; j++) {
+				if (a[j] < temp) {
+					temp = a[j];
+					flag = j;
 				}
 			}
-			if (flag!=i) {
-				a[flag]=a[i];
-				a[i]=temp;
+			//当某个位置的元素执行for循环判断时，发现自己是最小值时，此时不需要执行无必要的交换操作。
+			if (flag != i) {
+				a[flag] = a[i];
+				a[i] = temp;
 			}
 		}
 	}
--- src/main/java/com/rhyme/java程序员面试笔试宝典/part8/选择排序B.java
+++ src/main/java/com/rhyme/java程序员面试笔试宝典/part8/选择排序B.java
@@ -1,28 +1,38 @@
 package com.rhyme.java程序员面试笔试宝典.part8;
 
+import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * 
+ * @deprecated 对于给定的一组记录，经过第一轮比较后得到最小的记录，然后将该记录与第一个记录的位置进行交换。
+ *             接着对不包括第一个的记录进行第二轮比较，得到最小的记录与第二个记录进行交换。重复该过程
+ *             效率较低。10W数据，耗时2秒左右。50W数据耗时1分钟左右。100W数据，三分钟。
+ * 
+ * @author rhyme
+ */
 public class 选择排序B {
 	public static void selectSort(int a[]) {
 		for (int i = 0; i < a.length; i++) {
-			for (int j = i+1; j < a.length; j++) {
-				int temp=a[i];
-				if (a[i]>a[j]) {
-					a[i]=a[j];
-					a[j]=temp;
+			for (int j = i + 1; j < a.length; j++) {
+				// 非常糟糕的选择排序实现方式
+				int temp = a[i];
+				if (a[i] > a[j]) {
+					a[i] = a[j];
+					a[j] = temp;
 				}
 			}
 		}
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
-		}
 	}
-	public static void main(String[] args) {	
+
+	public static void main(String[] args) {
 		int j = 10;
 		int[] a = new int[j];
 		for (int i = 0; i < j; i++) {
 			a[i] = new Random().nextInt(j);
 		}
+		System.out.println(Arrays.toString(a));
 		selectSort(a);
+		System.out.println(Arrays.toString(a));
 	}
 }
